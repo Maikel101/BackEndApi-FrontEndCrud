57ea84062d39e5ee88755fbbd4faab0e73987fed
Maikel101 <miguel.rol@outlook.es>
2024-05-10 21:04:46 +0200

    Primer commit

---
+++ BBDD&INSERTS.sql
@@ -0,0 +1,30 @@
+
+
+create database DBEmpleado
+
+use DBEmpleado
+
+create table Departamento(
+IdDepartamento int primary key identity,
+Nombre varchar(50),
+FechaCreacion datetime default getdate()
+)
+
+create table Empleado(
+IdEmpleado int primary key identity,
+NombreCompleto varchar(50),
+IdDepartamento int references Departamento(IdDepartamento),
+Sueldo int,
+FechaContrato datetime,
+FechaCreacion datetime default getdate()
+)
+
+insert into Departamento(Nombre) values
+('Administración'),
+('Marketing'),
+('Ventas'),
+('Comercio')
+
+
+insert into Empleado(NombreCompleto, IdDepartamento, Sueldo, FechaContrato) values
+('Miguel roldán', 1, 3000, getdate())
